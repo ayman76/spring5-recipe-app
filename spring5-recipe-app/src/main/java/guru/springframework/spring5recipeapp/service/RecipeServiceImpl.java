4a33f83098490705cb95a371f0f33c69bbdc0f3b
Ayman <a.m_ayman9@outlook.com>
2023-05-25 19:38:17 +0200

    Adding Create and Update functions and add html page for them

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/service/RecipeServiceImpl.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/service/RecipeServiceImpl.java
@@ -60,4 +60,11 @@ public class RecipeServiceImpl implements RecipeService {
         return recipeToRecipeCommand.convert(savedRecipe);
     }
 
+    @Override
+    public RecipeCommand findCommandById(Long id) {
+        return recipeToRecipeCommand.convert(findById(id));
+    }
+
+    
+
 }
