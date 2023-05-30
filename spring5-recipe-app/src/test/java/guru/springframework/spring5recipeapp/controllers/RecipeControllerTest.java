e56b4efe3606fb5d243ef377a485f7b40c0a3c34
Ayman <a.m_ayman9@outlook.com>
2023-05-30 17:43:26 +0300

    passing exception attribute to 404error page

--- spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -103,7 +103,8 @@ public class RecipeControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/show"))
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andExpect(MockMvcResultMatchers.view().name("404error"));
+                .andExpect(MockMvcResultMatchers.view().name("404error"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("exception"));
     }
 
 }
