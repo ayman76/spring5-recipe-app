6bab68c17e4f23150f2d8b9350eed158d9611682
Ayman <a.m_ayman9@outlook.com>
2023-05-30 17:37:46 +0300

    Adding 404error page

--- spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -102,7 +102,8 @@ public class RecipeControllerTest {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/show"))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.view().name("404error"));
     }
 
 }
